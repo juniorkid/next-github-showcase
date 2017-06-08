609fefa7edde45689667392c446524b5d152d911
Siriwat Kunaporn <siriwatkunaporn@gmail.com>
2017-06-08 17:54:33 +0700

    add repo page

--- hocs/withRepos.js
+++ hocs/withRepos.js
@@ -1,73 +1,73 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 const api = (org) => {
-    return new Promise((resolve ,reject)=>{
-        setTimeout(()=>{
-            if(org === 'facebook'){
-                return resolve([
-                    {
-                        name:'nook',
-                        id:'4',
-                        language:'en',
-                        html_url:'http//google.com'
-                    },
-                    {
-                        name: 'kkkk',
-                        id:'3',
-                        language:'en',
-                        html_url:'http//google.com'
-                    }
-                ])
-                   }
-        } , 2000    )
-    })
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (org === 'facebook') {
+        return resolve([
+          {
+            name    : 'nook',
+            id      : '4',
+            language: 'en',
+            html_url: 'http//google.com',
+          },
+          {
+            name    : 'kkkk',
+            id      : '3',
+            language: 'en',
+            html_url: 'http//google.com',
+          },
+        ]);
+      }
+    }, 2000);
+  });
+};
 
 const withRepos = (ComposedComponent) => (
   class extends Component {
-    
+
     state = {
       isLoading: false,
-      data: []
-    }
+      data     : [],
+    };
 
     searchRepoWithOrganizeName = (orgName) => {
       this.setState({
-          isLoading: true,
-          data: []
-      })
+        isLoading: true,
+        data     : [],
+      });
 
       // fetch(`https://api.github.com/orgs/${orgName}/repos`)
       api(orgName)
       // .then(res => res.json())
       .then(res => {
-          console.log('res',res)
+        console.log('res', res);
         this.setState({
           isLoading: false,
-          data: res
-        })
+          data     : res,
+        });
       })
       .catch(error => {
         this.setState({
           isLoading: false,
-          data: null
-        })
-      })
-    }
-    
-    render () {
-      const { isLoading, data } = this.state
-      
+          data     : null,
+        });
+      });
+    };
+
+    render() {
+      const { isLoading, data } = this.state;
+
       return (
-        <ComposedComponent 
-          isFetchingRepo={isLoading} 
-          repoList={data} 
-          searchRepoWithOrganizeName={this.searchRepoWithOrganizeName} 
+        <ComposedComponent
+          isFetchingRepo={isLoading}
+          repoList={data}
+          searchRepoWithOrganizeName={this.searchRepoWithOrganizeName}
           {...this.props}
         />
-      )
+      );
     }
   }
-)
+);
 
-export default withRepos
+export default withRepos;
--- pages/repos.js
+++ pages/repos.js
@@ -1,13 +1,82 @@
-import React from 'react'
-import {Container} from 'semantic-ui-react'
-import withPage from '../hocs/withPage'
+import React from 'react';
+import { compose } from 'recompose';
+import { Container } from 'semantic-ui-react';
+import withPage from '../hocs/withPage';
+import {
+  Grid,
+  Image,
+  Card,
+  Icon,
+  Segment,
+  Header,
+  Dimmer,
+  Loader
+,} from 'semantic-ui-react';
+import { withRepo } from '../hocs/withRepo';
 
-const ReposPage = () => {
-  return (
-    <Container>
-      ReposPage
-    </Container>
+const ReposPage = compose(
+  withRepo(),
+  withPage('repos page')
+)(
+  (
+    ({
+      repoValue: {
+        id,
+        name,
+        icon,
+        description,
+        html_url,
+        language,
+      },
+      status,
+    }) => {
+      return (
+        <Container>
+          {
+            status === 'request' ?
+              <Dimmer active inverted>
+                <Loader inverted>Loading</Loader>
+              </Dimmer>
+              :
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={4}>
+                    <Card>
+                      <Image src={icon} />
+                      <Card.Content>
+                        <Card.Header>
+                          {name}
+                        </Card.Header>
+                        <Card.Meta>
+                        <span className='date'>
+                          {id}
+                        </span>
+                        </Card.Meta>
+                        <Card.Description>
+                          {html_url}
+                        </Card.Description>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <a>
+                          <Icon name='user' />
+                          {language}
+                        </a>
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+                  <Grid.Column width={12}>
+                    <Segment>
+                      <Header as="h2">Description</Header>
+                      <Header as="h4">{description}</Header>
+                    </Segment>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+          }
+        </Container>
+      );
+    }
   )
-}
+);
 
-export default withPage('repos page')(ReposPage)
+export default (ReposPage);
